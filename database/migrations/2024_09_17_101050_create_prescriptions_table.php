da3fc81bc219f6bd8109f6a359613d076540f82e
Emmar Caber <emmar.caber@coreproc.com>
2024-09-17 18:30:28 +0800

    feat: create prescriptions table
    This table is the pivot table between the doctors, drugs, and patients table

---
+++ database/migrations/2024_09_17_101050_create_prescriptions_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->string('drug_trade_name');
+            $table->unsignedBigInteger('doctor_id');
+            $table->date('date_prescribed');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('patient_id')
+                ->references('PID')
+                ->on('patients')
+                ->onDelete('cascade');
+
+            $table->foreign('drug_trade_name')
+                ->references('trade_name')
+                ->on('drugs')
+                ->onDelete('cascade');
+
+            $table->foreign('doctor_id')
+                ->references('phys_id')
+                ->on('doctors')
+                ->onDelete('cascade');
+
+
+            $table->unique(['patient_id', 'drug_trade_name', 'doctor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('prescriptions', function (Blueprint $table) {
+            $table->dropForeign(['patient_id']);
+            $table->dropForeign(['drug_trade_name']);
+            $table->dropForeign(['doctor_id']);
+        });
+
+        Schema::dropIfExists('doctor_prescribe_patient_drug');
+    }
+};
