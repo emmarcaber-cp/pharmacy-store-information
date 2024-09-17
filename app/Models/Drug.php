ae050263ea8506fa77326ea358dbc7378c7a36b5
Emmar Caber <emmar.caber@coreproc.com>
2024-09-17 15:51:55 +0800

    Feature [#4] Create Drug Model and Migration (#10)
    * feat: create drugs model and migration
    Establish also the relationship between drug manufacturers and drugs
    
    * fix: fillable error in drug model

---
+++ app/Models/Drug.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Drug extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'trade_name',
+        'drug_manufacturer_id',
+    ];
+
+    public function manufacturer(): BelongsTo
+    {
+        return $this->belongsTo(DrugManufacturer::class);
+    }
+}
--- app/Models/DrugManufacturer.php
+++ app/Models/DrugManufacturer.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DrugManufacturer extends Model
 {
@@ -13,4 +14,9 @@ class DrugManufacturer extends Model
         'name',
         'address',
     ];
+
+    public function drugs(): HasMany
+    {
+        return $this->hasMany(Drug::class);
+    }
 }
