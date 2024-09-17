c4684d9ac40a20e27487f77b1f44256f1a1ff972
Emmar Caber <emmar.caber@coreproc.com>
2024-09-17 15:22:49 +0800

    feat: create doctor model and migrations

---
+++ database/migrations/2024_09_17_071734_create_doctors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('phys_id');
+            $table->string('d_name');
+            $table->string('specialty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
