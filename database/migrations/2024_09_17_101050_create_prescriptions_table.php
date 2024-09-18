c5c917f972b6d5c2346c2696e9d433937560c55b
Emmar Caber <emmar.caber@coreproc.com>
2024-09-18 15:43:45 +0800

    Refactor prescriptions table migration to drop 'prescriptions' table

--- database/migrations/2024_09_17_101050_create_prescriptions_table.php
+++ database/migrations/2024_09_17_101050_create_prescriptions_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('doctor_prescribe_patient_drug');
+        Schema::dropIfExists('prescriptions');
     }
 };
