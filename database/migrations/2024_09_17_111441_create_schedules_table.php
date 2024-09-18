ee735c4297e5b7146cd5d3d2c1b0cef7c0140e1c
Emmar Caber <emmar.caber@coreproc.com>
2024-09-18 15:47:05 +0800

    Refactor migration to create 'schedules' table instead of 'works'

--- database/migrations/2024_09_17_111441_create_schedules_table.php
+++ database/migrations/2024_09_17_111441_create_schedules_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('works', function (Blueprint $table) {
+        Schema::create('schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('employee_id')->constrained();
             $table->foreignId('pharmacy_id')->constrained();
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('works');
+        Schema::dropIfExists('schedules');
     }
 };
