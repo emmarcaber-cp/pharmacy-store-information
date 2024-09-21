9a373990f92db6be56a6962fdb06ad478844d6eb
Emmar Caber <emmar.caber@coreproc.com>
2024-09-21 19:56:40 +0800

    Refactor UserSeeder to dynamically create users for different models

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,52 +18,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $doctor = Doctor::factory()->create();
-        $patient = Patient::factory()->create();
-        $drugManufacturer = DrugManufacturer::factory()->create();
-        $employee = Employee::factory()->create();
-        $pharmacy = Pharmacy::factory()->create();
-
-        $users = [
-            [
-                'name' => 'Patient User',
-                'email' => 'patient_user@email.com',
-                'password' => bcrypt('pass123'),
-                'auth_id' => $patient->id,
-                'auth_type' => Patient::class,
-            ],
-            [
-                'name' => 'Doctor User',
-                'email' => 'doctor_user@email.com',
-                'password' => bcrypt('pass123'),
-                'auth_id' => $doctor->id,
-                'auth_type' => Doctor::class,
-            ],
-            [
-                'name' => 'Drug Manufacturer User',
-                'email' => 'drug_manufacturer_user@email.com',
-                'password' => bcrypt('pass123'),
-                'auth_id' => $drugManufacturer->id,
-                'auth_type' => DrugManufacturer::class,
-            ],
-            [
-                'name' => 'Employee User',
-                'email' => 'employee_user@email.com',
-                'password' => bcrypt('pass123'),
-                'auth_id' => $employee->id,
-                'auth_type' => Employee::class,
-            ],
-            [
-                'name' => 'Pharmacy User',
-                'email' => 'pharmacy_user@email.com',
-                'password' => bcrypt('pass123'),
-                'auth_id' => $pharmacy->id,
-                'auth_type' => Pharmacy::class,
-            ],
+        $userTypes = [
+            Doctor::class => ['name' => 'Doctor User',],
+            Patient::class => ['name' => 'Patient User'],
+            DrugManufacturer::class => ['name' => 'Drug Manufacturer User'],
+            Employee::class => ['name' => 'Employee User'],
+            Pharmacy::class => ['name' => 'Pharmacy User'],
         ];
 
-        foreach ($users as $user) {
-            User::firstOrCreate(['email' => $user['email']], $user);
+        foreach ($userTypes as $model => $userInformation) {
+            $modelInstance = $model::factory()->create($userInformation);
+            $userName = $userInformation['name'];
+
+            User::firstOrCreate(
+                ['email' => strtolower(str_replace(' ', '_', $userName)) . '@email.com'],
+                [
+                    'name' => $userName,
+                    'password' => bcrypt('pass123'),
+                    'auth_id' => $modelInstance->id,
+                    'auth_type' => $model,
+                ]
+            );
         }
     }
 }
