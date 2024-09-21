19f93c46d9462a4de5207c30e1a6b194649cab7e
Emmar Caber <emmar.caber@coreproc.com>
2024-09-21 19:17:26 +0800

    Refactor Doctor model to include AutoCreatesAuthFields trait

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\AutoCreatesAuthFields;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -15,7 +16,7 @@ use Illuminate\Database\Eloquent\Relations\MorphOne;
  */
 class Doctor extends Model
 {
-    use HasFactory;
+    use HasFactory, AutoCreatesAuthFields;
 
     protected $fillable = [
         'name',
